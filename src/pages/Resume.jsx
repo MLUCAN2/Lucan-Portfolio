49cc5f9ecd29357b414fbb7d6f785dd42f83b7e9
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-06-13 12:25:42 -0500

    updated resume section

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,25 +1,32 @@
 import React from "react";
+import resume from '../../img/resume.png';
 
 const Resume= ()=> (
     <section className="resume-section">
-        <div className="my-resume">
-            <h2>Resume</h2>
-            <a href="docs/Resume.pdf" download>Download Resume</a>
-        </div>
-        <div className="proficiencies">
-            <h3>My Proficiencies</h3>
-            <ul>
-                <li>UI/UX Design</li>
-                <li>HTML/CSS</li>
-                <li>JavaScript</li>
-                <li>Node.js</li>
-                <li>React.js</li>
-                <li>MySQL</li>
-                <li>Database Management</li>
-            </ul>
-        </div>
-    </section>
+    <div className="resume-container">
+        <img src={resume} alt="My Resume" className="resume-image" />
+        <a href='docs/Resume.pdf' className="resume-link" download>Download Resume</a>
+    </div>
+</section>
 );
 
 export default Resume;
 
+// <section className="resume-section">
+// <div className="my-resume">
+//     <h2>Resume</h2>
+//     <a href="docs/Resume.pdf" download>Download Resume</a>
+// </div>
+// <div className="proficiencies">
+//     <h3>My Proficiencies</h3>
+//     <ul>
+//         <li>UI/UX Design</li>
+//         <li>HTML/CSS</li>
+//         <li>JavaScript</li>
+//         <li>Node.js</li>
+//         <li>React.js</li>
+//         <li>MySQL</li>
+//         <li>Database Management</li>
+//     </ul>
+// </div>
+// </section>
