aeb3013db900af8d9d3e297c1810c483e31b0159
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-07-10 12:06:18 -0500

    added tech stacks to the project cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,13 @@
+
 import React from "react";
 
-const Project = ({ title, image, appLink, repoLink }) => (
+const Project = ({ title, image, techStack, appLink, repoLink }) => (
     <div className="project">
       <div className="project-images">
         <div className="project-card">
           <img src={image} alt={`${title}-site`} className="project-image" />
           <div className="project-overlay">
+            <a className="techStack">Tech Stack: {techStack}</a>
             <a href={appLink} target="_blank" rel="noopener noreferrer">The App</a>
             <a href={repoLink} target="_blank" rel="noopener noreferrer">Git Repo</a>
           </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,41 +14,48 @@ const Portfolio = () => {
       {
         title: 'Round-Abouts',
         image: roundAbouts,
+        techStack: 'MERN',
         appLink: 'https://round-abouts.onrender.com',
         repoLink: 'https://github.com/MLUCAN2/round-abouts.git'
       },
       {
         title: 'Meal-Plan',
         image: mealPlan,
+        techStack: 'HTML, CSS, JavaScript, Third-Party API',
         appLink: 'https://devunte.github.io/Meal-Plan/',
         repoLink: 'https://github.com/Devunte/Meal-Plan.git'
       },
       {
         title: 'Dev-Seeker',
         image: devSeeker,
+        techStack: 'Node, Express, Handlebars, PostgreSQL',
         appLink: ' https://tech-portfolio-site.onrender.com/',
         repoLink: 'https://github.com/KyleH-Git/tech-portfolio-site.git'
       },
       {
         title: 'Weather App',
         image: weather,
+        techStack: 'HTML, CSS, JavaScript, Third-Party API',
         appLink:'https://mlucan2.github.io/my-weather-app/',
         repoLink: 'https://github.com/MLUCAN2/my-weather-app'
       },
       {
         title: 'Task Board',
         image: kanban,
+        techStack: 'HTML, CSS, JavaScript',
         appLink: 'https://mlucan2.github.io/team-todo-board/',
         repoLink: 'https://github.com/MLUCAN2/team-todo-board.git'
       },
       {
         title: 'ReadMe Generator',
         image: myReadMe,
+        techStack: 'Express, Node',
         repoLink: 'https://github.com/MLUCAN2/my-read-me.git'
       },
       {
         title: 'SVG Logo Generator',
         image: logoGenerator,
+        techStack: 'SVG, Express, Node',
         repoLink: 'https://github.com/MLUCAN2/logo-generator.git'
       }
   
@@ -61,6 +68,7 @@ const Portfolio = () => {
             key={index} 
             title={project.title} 
             image={project.image} 
+            techStack={project.techStack}
             appLink={project.appLink} 
             repoLink={project.repoLink} 
           />
